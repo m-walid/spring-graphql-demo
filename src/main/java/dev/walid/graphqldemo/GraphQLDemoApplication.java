ed9b7bbdeb35e8790ff8035b9aadaed35746a1b7
Mohamed Walid Elbendary <mohamed.elbendary@valeo.com>
2023-08-27 10:41:37 +0300

    fix main method name

--- src/main/java/dev/walid/graphqldemo/GraphQLDemoApplication.java
+++ src/main/java/dev/walid/graphqldemo/GraphQLDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GraphqldemoApplication {
+public class GraphQLDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GraphqldemoApplication.class, args);
+        SpringApplication.run(GraphQLDemoApplication.class, args);
     }
 
 }
